a01644318842715511cbdce8d119d4e39200a316
Enngage <ennspassion@gmail.com>
2017-10-02 10:46:08 +0200

    removes default test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Kentico Cloud Angular');
-  }));
 });
